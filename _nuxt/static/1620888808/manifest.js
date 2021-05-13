6d723c68efd46343407dc9cdf94b8889791bc366
flat35hd99 <flat35hd99@users.noreply.github.com>
2021-05-13 06:53:30 +0000

    deploy: e35005d6792bdf82ece5dbd670d66ecbb0ed2623

---
+++ _nuxt/static/1620888808/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FoptimizationOfSystemsWithUltraManyDegreesOfFreedom","\u002Fhow_to_use_tools\u002Fgit_and_github","\u002Fslides"]})
